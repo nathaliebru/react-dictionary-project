29e3d7c63539f8c9212aba348b8d506a4f07dfb7
nathaliebru <92443285+nathaliebru@users.noreply.github.com>
2022-01-03 22:41:40 +0100

    Added photo search with Pexels API

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,32 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
     event.preventDefault();
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let pexelsApiKey =
+      "563492ad6f917000010000012aba537122054e85b5432ed9a8f11e93";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeyword(event) {
@@ -28,8 +40,8 @@ export default function Dictionary() {
         <input type="search" onChange={handleKeyword} />
       </form>
       <div className="hint">i.e. coding, coffee, wine, fitness...</div>
-
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
